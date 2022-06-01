682c452fd8dcba9a0d4585642f4065c955b3928b
khoado2410 <khoado24@gmail.com>
2022-06-01 13:04:50 +0700

    commit

---
+++ src-microfrontends/micro-frontend-product/src/app/product-detail/header-host/header-host.component.ts
@@ -0,0 +1,27 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { Component, ComponentRef, OnInit, ViewContainerRef } from '@angular/core';
+import { LayoutErrorComponent } from 'src/app/handle-error/layout-error/layout-error.component';
+
+@Component({
+  selector: 'app-header-host',
+  templateUrl: './header-host.component.html',
+  styleUrls: ['./header-host.component.scss']
+})
+export class HeaderHostComponent implements OnInit {
+  constructor(
+    private vcr: ViewContainerRef
+  ) { }
+
+  async ngOnInit() {
+    const { HeaderComponent } = await loadRemoteModule({
+      remoteEntry: 'http://localhost:8000/remoteEntry.js',
+      exposedModule: './HeaderComponent',
+      type: 'module'
+    })
+      .catch(err => {
+        this.vcr.createComponent(LayoutErrorComponent)
+      })
+    this.vcr.createComponent(HeaderComponent);
+  }
+
+}
---
+++ src-microfrontends/micro-frontend-product/webpack.config.js
@@ -0,0 +1,57 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, 'tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "mf-products",
+    publicPath: "auto"
+  },
+  optimization: {
+    runtimeChunk: false
+  },   
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        library: { type: "module" },
+
+        // For remotes (please adjust)
+        name: "mf-products",
+        filename: "remoteEntry.js",
+        exposes: {
+             './ProductsModule': path.join(__dirname, 'src') + '/app/products/products.module.ts',
+             './ProductDetailModule': './/src/app/product-detail/product-detail.module.ts',
+             './AddProductModule': './/src/app/add-product/add-product.module.ts'
+        },        
+        
+        // For hosts (please adjust)
+        remotes: {
+          "mf-layer": "http://localhost:8000/remoteEntry.js",
+        },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+        
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
